bfb9f012579573a6d3090c16c07d2628ae8aa0c1
Raul Rodrigues <raulrm.dev@gmail.com>
2022-08-28 15:46:46 -0300

    html tuning and javascript evolution

--- main.js
+++ main.js
@@ -1,37 +1,45 @@
-//const chosenNumber = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+/* Missing add button to refresh the page. I think about adding a button in the HTML, hiding the button in the CSS, in the JS after 3 attempts or after hitting the number, hiding input and displaying refresh.
+*/
 let input = document.getElementById("insert_number");
 let button = document.getElementById("button");
-const chosenNumber = 3;
 
 let correct = false;
 insert_number.oninput = function(){
     input.innerHTML = this.value;
 }
 
+const Guess={
+    number_attempt:0,
+}
+
+const chosen_number = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+
 function analysis(){
+
     let attempt = insert_number.value ;
-    for(let counter = 0; counter < 2; counter++){
 
-        if(attempt>10){
+        if(Guess.number_attempt>1){
+            alert(`Infelizmente, você não acertou. O número era ${chosen_number}`);
+            Guess.number_attempt=0;
+        }
+
+        else if(attempt>10){
             alert('Valor inválido! Tente um número de 0 a 10');
+            ++Guess.number_attempt;
         }
 
-        if(attempt<chosenNumber){
-            alert(`Errado! Tente um número > que ${attempt} e <= 10`);;
+        else if(attempt<chosen_number){
+            alert(`Errado! Tente um número > que ${attempt} e <= 10`);
+            ++Guess.number_attempt;
         }
 
-        if(attempt>chosenNumber){
-            alert(`Errado! Tente um número < que ${attempt} e >= 0`);;
+        else if(attempt>chosen_number){
+            alert(`Errado! Tente um número < que ${attempt} e >= 0`);
+            ++Guess.number_attempt;
         }
 
-        if(attempt == chosenNumber){
-            alert(`Parabéns, você acertou! O número era ${chosenNumber}.`);
+        else if(attempt == chosen_number){
+            alert(`Parabéns, você acertou! O número era ${chosen_number}.`);
             correct = true;
-            break;
         }
-
-    }
-    if(!correct){
-        alert(`Infelizmente, você não acertou. O número era ${chosenNumber}`);
-    }
 }
